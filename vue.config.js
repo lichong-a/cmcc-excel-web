d899e4882de50fbd5941721f56f5a8c1a6870068
ric <git@lichong.host>
2021-12-09 14:47:47 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,22 @@ function resolve(dir){
     return path.join(__dirname,dir)//path.join(__dirname)设置绝对路径
 }
 module.exports = {
+  //是否使用包含运行时编译器的 Vue 构建版本。
   runtimeCompiler: true,
+  // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
+  productionSourceMap: false,
+  //当运行 vue-cli-service build 时生成的生产环境构建文件的目录
+  outputDir: 'dist',
+  //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: 'assets',
+  // eslint-loader 是否在保存的时候检查 安装@vue/cli-plugin-eslint有效
+  lintOnSave: true,
   devServer: {
-    open: true,
+    hot: true, //热加载
+    host: '0.0.0.0', //ip地址
+    port: 8085, //端口
+    https: false, //false关闭https，true为开启
+    open: true, //自动打开浏览器
     proxy:{
       '/api': {
         target: 'http://localhost:8080',
